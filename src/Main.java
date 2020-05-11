46e837a1374b6ef8df0be517e8af4dc695a0cf9f
wuaidedidi <470357714@qq.com>
2020-05-11 22:03:15 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World222! ");
-        System.out.println("2222");
+
+                System.out.println("2222");
     }
 }
