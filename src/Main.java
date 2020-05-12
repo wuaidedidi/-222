0961b2caebba32de00756cdcf9242b2fb7e09548
wuaidedidi <470357714@qq.com>
2020-05-12 08:55:00 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,14 @@ public class Main {
         System.out.println("大白兔 ");
 //        System.out.println("大白兔 ");
     }
+    public void dabaitu1(){
+        System.out.println("大白兔 ");
+        System.out.println("大白兔 ");
+//        System.out.println("大白兔 ");
+    }
+    public void dabaitu2(){
+        System.out.println("大白兔 ");
+        System.out.println("大白兔 ");
+//        System.out.println("大白兔 ");
+    }
 }
