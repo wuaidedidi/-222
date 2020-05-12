7b1df14a7dd498dbb22318051db58fb988b48d6c
wuaidedidi <470357714@qq.com>
2020-05-12 09:05:16 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,11 @@ public class Main {
         System.out.println("大白兔 ");
 //        System.out.println("大白兔 ");
     }
+
     public void dabaitu1(){
         System.out.println("大白兔 ");
         System.out.println("大白兔 ");
 //        System.out.println("大白兔 ");
     }
-    public void dabaitu2(){
-        System.out.println("大白兔 ");
-        System.out.println("大白兔 ");
-//        System.out.println("大白兔 ");
-    }
+
 }
